785d1b66aebe9f7ee34f4135cc5e5664aec37983
justin890913-sys <justin890913@gmail.com>
2026-02-03 17:52:21 +0800

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBO1QITelhIzyFze8hmdwkw-bbcBg21av8",
   authDomain: "expensesplitter-50167.firebaseapp.com",
@@ -12,7 +16,6 @@ const firebaseConfig = {
   measurementId: "G-JQYEWX4W5H"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
