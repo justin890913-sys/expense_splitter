321b1858d42c50691cecf334d0edc6432d99bf5b
justin890913-sys <justin890913@gmail.com>
2026-02-03 18:31:20 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import ExpenseSplitter from "../components/ExpenseSplitter";
 
 export default function Home() {
   return (
-    <div className="bg-gray-50 min-h-screen pt-6">
+    <div className="w-full h-full">
       <ExpenseSplitter />
     </div>
   );
